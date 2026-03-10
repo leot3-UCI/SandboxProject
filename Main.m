4330b1da6ec2d3588535b7d3030657e5be72f87d
leot3-UCI <leot3@uci.edu>
2026-03-10 12:23:42 -0700

    Created the Base file
    Finished the base function and converted the image to grayscale

--- Main.m
+++ Main.m
@@ -1,2 +1,37 @@
 %% Image To Text Art Converter
 % BME 60B Sandbox Project
+% Members: Leo Tian, Khendra Beth Hernandez, Jericho Celeste, Isabel Bueno
+
+clc;
+clear;
+close all;
+
+%% Part 1
+% Convert the image of the dog to grayscale
+
+% read image
+img = imread('AHHHDog.jpg');
+
+% convet to grayscale
+grayImg = rgb2gray(img);
+
+% Show original
+figure;
+subplot(1,2,1);
+imshow(img);
+title('Original Image');
+
+% Show grayscale
+subplot(1,2,2);
+imshow(grayImg);
+title('Grayscale Image');
+
+
+%% Part 2
+% Reduce the resolution of the image
+
+%% Part 3
+% Map brightness to character density
+
+%% Part 4
+% Print out text art
